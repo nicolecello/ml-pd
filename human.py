cb7d9d9d97eda069ec667c687eb892ff1d2ab67d
Nicole Wang <nicole.wang@leidos.com>
2021-05-11 12:29:43 +1000

    virtualenv setup for box2d

--- human.py
+++ human.py
@@ -1,11 +1,14 @@
 import gym
-env = gym.make("CartPole-v1")
+env = gym.make("BipedalWalker-v3")
 observation = env.reset()
 for _ in range(1000):
   env.render()
   action = env.action_space.sample() # your agent here (this takes random actions)
   observation, reward, done, info = env.step(action)
-
+  print(f'observation: {observation}')
+  print(f'reward: {reward}')
+  print(f'done: {done}')
   if done:
+    print("done")
     observation = env.reset()
 env.close()
